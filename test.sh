5a6a8f58200d625e7a7a12a9a45971dfe3d78d51
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2015-02-16 21:09:10 +0100

    shell: add glmark target

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 function ioctl_test {
   local libmali="${HOME}/local/lib/mali-r4p0-fbdev"
+  local glmark="${HOME}/local/bin/glmark2-es2"
 
   case "${1}" in
     "trace" )
@@ -14,6 +15,9 @@ function ioctl_test {
     "hooktrace" )
       touch "/dev/shm/fake_fbdev"
       LD_PRELOAD=./hook.so LD_LIBRARY_PATH=$libmali strace ./test 2> trace.out ;;
+    "glmark" )
+      touch "/dev/shm/fake_fbdev"
+      LD_PRELOAD=./hook.so LD_LIBRARY_PATH=$libmali $glmark ;;
     * )
       LD_LIBRARY_PATH=$libmali ./test ;;
     esac
